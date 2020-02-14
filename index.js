bf784a857089387f684c9169baa69a395a0dec64
Daniel Pittman <dpdanpittman@gmail.com>
2020-02-13 21:56:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1640,10 +1640,10 @@ function daily(addr) {
                         break;
                     }
                 } catch(e) {
-                    console.log('An affliction happened ', e.message)
+                    console.log('An affliction happened', e.message)
                    }
-                   
                 }}
+                try {
               if (state.land[addr].care[i][1] == 'harvested'){
                 if (!harvested && state.land[addr].stage > 7){
                   harvested = true
@@ -1675,6 +1675,11 @@ function daily(addr) {
                   }
                   state.land[addr] = parcel
                   
-                }}}
+                }}
+                } catch(e) {
+                    console.log('harvesting error', e.message)
+                   }
+            
+            }
                   cleanup(addr, cleanupindex)
                 }}
