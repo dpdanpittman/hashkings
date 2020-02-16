35b6bd5ab8fe0cbf521e7618182226c5595adebf
Daniel Pittman <dpdanpittman@gmail.com>
2020-02-15 20:20:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -302,17 +302,17 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS token API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 40859610; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 40859505; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = steem.PrivateKey.from(ENV.KEY); //active key for account
 const sh = ENV.sh || '';
-const ago = ENV.ago || 40859610;
+const ago = ENV.ago || 40859505;
 const prefix = ENV.PREFIX || 'hk_'; // part of custom json visible on the blockchain during watering etc..
 const clientURL = ENV.APIURL || 'https://api.steemit.com' // can be changed to another node
 var client = new steem.Client(clientURL);
 var processor;
 var recents = [];
-//const transactor = steemTransact(client, steem, prefix);
+const transactor = steemTransact(client, steem, prefix);
 
 /****ISSUE****/
 //I think this is where the app can get the hash from qwoyn_report that is saved in state.js and use it
