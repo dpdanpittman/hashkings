93bb3e59c6614a70112a163a2367e643c468680a
Daniel Pittman <dpdanpittman@gmail.com>
2020-02-24 21:04:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -962,8 +962,8 @@ function startApp() {
                         pollinated: true
                     }
                     //////
-
-                    state.users[state.land[addr].owner].addr.push(pol)
+                    state.land[addr].pollinated = true
+                    //state.users[state.land[addr].owner].addr.push(pol)
                     
                 }
                 } catch (e){
