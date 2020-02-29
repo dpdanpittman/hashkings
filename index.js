b7128bd66a292957129053bcd657a3a1ce8163a3
Daniel Pittman <dpdanpittman@gmail.com>
2020-02-29 01:40:49 -0800

    update

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ app.listen(port, () => console.log(`HASHKINGS token API listening on port ${port
 var state;
 var startingBlock = ENV.STARTINGBLOCK || 41238843; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
-const key = steem.PrivateKey.from(ENV.KEY); //active k      ey for account
+const key = steem.PrivateKey.from(ENV.KEY); //active key for account
 const sh = ENV.sh || '';
 const ago = ENV.ago || 41238843;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
@@ -371,7 +371,7 @@ steemjs.api.getAccountHistory(username, -1, 100, function(err, result) {
     console.log(err)
     startWith(sh)
   } else {
-    let ebus = result.filter( tx => tx[1].op[1].id === 'hashkings_report' )
+    let ebus = result.filter( tx => tx[1].op[1].id === 'qwoyn_report' )
     for(i=ebus.length -1; i>=0; i--){
       if(JSON.parse(ebus[i][1].op[1].json).stateHash !== null)recents.push(JSON.parse(ebus[i][1].op[1].json).stateHash)
     }
