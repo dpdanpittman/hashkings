41d69d04eebee676b5790a5596d38b5d8a37d143
Daniel Pittman <dpdanpittman@gmail.com>
2020-03-02 04:35:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -351,11 +351,11 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS token API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 41286859; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 41309032; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = steem.PrivateKey.from(ENV.KEY); //active key for account
 const sh = ENV.sh || '';
-const ago = ENV.ago || 41286859;
+const ago = ENV.ago || 41309032;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 const clientURL = ENV.APIURL || 'https://api.steemit.com' // can be changed to another node
 var client = new steem.Client(clientURL);
