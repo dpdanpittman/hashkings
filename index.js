94fe551fb9a492773c858b5d8248be473c950ac7
Daniel Pittman <dpdanpittman@gmail.com>
2020-03-02 00:10:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -938,7 +938,7 @@ function startApp() {
             try {
             if (state.land[plants[i]].owner == from) {
                 state.land[plants[i]].care.unshift([processor.getCurrentBlockNumber(), 'watered']);
-                plantnames -= `${plants[i]} `
+                plantnames += `${plants[i]} `
             }
             } catch (e){
               state.cs[`${json.block_num}:${from}`] = `${from} can't water what is not theirs`
