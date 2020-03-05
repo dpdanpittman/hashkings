321dbfd1290217c167f36f725674aa541c94c7f0
Daniel Pittman <dpdanpittman@gmail.com>
2020-03-04 22:43:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1170,11 +1170,18 @@ function startApp() {
         for(i = 0; i < 5; i++) {
         
         if(from=='hashkings'){state.users[json.to].seeds.push(seed + i)}
-        if(from=='hashkings'){state.users[json.to].seeds.push(pollen + i)}
-        if(from=='hashkings'){state.users[json.to].seeds.push(buds + i)}
+        if(from=='hashkings'){state.users[json.to].pollen.push(pollen + i)}
+        if(from=='hashkings'){state.users[json.to].buds.push(buds + i)}
+
+        if(from=='hashkings'){state.users[json.to].seeds.push(seed + i)}
+        if(from=='hashkings'){state.users[json.to].pollen.push(pollen + i)}
+        if(from=='hashkings'){state.users[json.to].buds.push(buds + i)}
+
+        if(from=='hashkings'){state.users[json.to].seeds.push(seed + i)}
+        if(from=='hashkings'){state.users[json.to].pollen.push(pollen + i)}
+        if(from=='hashkings'){state.users[json.to].buds.push(buds + i)}
         }
         state.cs[`${json.block_num}:${json.to}`] = `received monthly patreon tier3 reward` 
-        
     });
     
     processor.on('news', function(json, from) {
