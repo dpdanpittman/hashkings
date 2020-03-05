ece87a1be6699147a022356d138ff3c8af6061e7
Daniel Pittman <dpdanpittman@gmail.com>
2020-03-04 22:37:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1167,9 +1167,10 @@ function startApp() {
             father: 'sensimilla'
         }
         
-        for(i = 0; i < 5; i++)
+        for(i = 0; i < 5; i++) {
         var seed = seed + `${i}`
         if(from=='hashkings'){state.users[json.to].seeds.push(seed)}
+        }
         state.cs[`${json.block_num}:${json.to}`] = `received monthly patreon tier3 reward` 
         
     });
