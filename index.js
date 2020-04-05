5f5d9b24068ac2d531d7c03900d687fa964f9c75
Daniel Pittman <dpdanpittman@gmail.com>
2020-04-05 00:45:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -357,7 +357,7 @@ const key = steem.PrivateKey.from(ENV.KEY); //active key for account
 const sh = ENV.sh || '';
 const ago = ENV.ago || 42249349;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
-const clientURL = ENV.APIURL || 'https://api.steemit.com/' // can be changed to another node
+const clientURL = ENV.APIURL || 'https://steemseed-se.privex.io' //https://api.steemit.com/' // can be changed to another node
 var client = new steem.Client(clientURL);
 var processor;
 var recents = [];
