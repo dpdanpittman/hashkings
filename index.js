e827b28fbcf2033b0f0e2503d9fd902929606c1d
Daniel Pittman <dpdanpittman@gmail.com>
2020-04-18 13:49:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -624,7 +624,7 @@ processor.on('market_post_seed', function(json, from) {
             try {
             if (state.users.from[seed].owner === from && state.users.from.seed.forSale === false) {
                 state.users.from[seed].forSale = true;
-                seednames += `${seed}`
+                seednames += `${seed} `
                 //state.cs[`${json.block_num}:${from}`] = `${from} succesfully posted a ${seednames} seed for sale`
             }
             } catch (e){
