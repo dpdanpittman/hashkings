7895bff5d482987a529989a791b30c385352ab9d
Daniel Pittman <dpdanpittman@gmail.com>
2020-04-22 12:39:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -628,8 +628,8 @@ processor.on('market_post_seed', function(json, from) {
     try {
         for (var i = 0; i < seed.length; i++) {   
         try {
-            if (state.users[json.from].seeds[seed].owner === from) {
-                state.users[json.from].seeds[seed].forSale = true;
+            if (state.users[json.from].seeds[i].owner === from) {
+                state.users[json.from].seeds[i].forSale = true;
                 seednames += `${seed[i]} `;
 
                 const postedToMarket = {
@@ -651,7 +651,7 @@ processor.on('market_post_seed', function(json, from) {
         }
         }
     } catch {
-        (console.log(from + ' tried to post ' + seednames +' pollen for sale but an error occured'))
+        (console.log(from + ' tried to post a ' + seednames +' seed for sale but an error occured'))
     }
 
     state.cs[`${json.block_num}:${from}`] = `${from} succesfully posted a ${seednames} seed for sale for ${json.price} STEEM`
