c324fcb9a0892fb4d5ca459b8e35954f23bfff4d
Daniel Pittman <dpdanpittman@gmail.com>
2020-04-23 17:54:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -645,7 +645,7 @@ processor.on('market_post_seed', function(json, from) {
             forSale:  true,
     
         }
-        state.users[from].seeds[seed].push(postedForSale);
+        state.users[from].seeds[json.seed].push(postedForSale);
 
    /* try {
         for (var i = 0; i < seed.length; i++) {   
