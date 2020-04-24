003c6250e400b0fbc1e461345a5458f7de30345a
Daniel Pittman <dpdanpittman@gmail.com>
2020-04-23 23:55:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -742,7 +742,7 @@ processor.on('market_cancel_seed', function(json, from) {
             if (state.users[from].seeds[0][seednames].owner === from && state.users[from].seeds[0][seednames].forSale === true) {
 
                 // remove seed from market
-                cancelSeed = state.market.seeds[from].seeds[0][seednames].splice(0, 1)[0]
+                //cancelSeed = state.market.seeds[from].seeds[0][seednames].splice(0, 1)[0]
 
                 // set canceled seed forSale to false in users inventory
                 state.users[from].seeds[0][seednames].forSale = false;
